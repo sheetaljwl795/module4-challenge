9fe9d923acb002392cff8afee67f3a7594b60467
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-07-27 18:09:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+
+var startBtn = document.querySelector("#start");
+var 
+
+// startQuiz function
+function startQuiz() {
+    // show question number 1
+
+
+
+
+}
+
+
+// Add event listener to generate button
+
+startBtn.addEventListener("click", startQuiz);
+
